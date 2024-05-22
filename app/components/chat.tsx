44e822b7a143154a2f86af1e2e81fa4f84f26367
Nylesssss n <2337620979@qq.com>
2024-05-22 22:30:19 +0000

    修改开场白的预设提示词说明

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1456,9 +1456,7 @@ function _Chat() {
                   </div>
 
                   <div className={styles["chat-message-action-date"]}>
-                    {isContext
-                      ? Locale.Chat.IsContext
-                      : message.date.toLocaleString()}
+                    {isContext ? "开场白" : message.date.toLocaleString()}
                   </div>
                 </div>
               </div>

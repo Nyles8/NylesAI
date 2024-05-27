8ea1b699f824a67aca7d5cd0e8dc6fe24a495838
Nylesssss <133832044+Nylesssss@users.noreply.github.com>
2024-05-27 10:21:27 +0800

    image.svg

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -336,7 +336,7 @@ function ClearContextDivider() {
     </div>
   );
 }
-
+//从此处开始修改
 function ChatAction(props: {
   text: string;
   icon: JSX.Element;
@@ -360,6 +360,11 @@ function ChatAction(props: {
     });
   }
 
+  // 在组件挂载时更新宽度
+  useEffect(() => {
+    updateWidth();
+  }, []);
+
   return (
     <div
       className={`${styles["chat-input-action"]} clickable`}
@@ -367,8 +372,6 @@ function ChatAction(props: {
         props.onClick();
         setTimeout(updateWidth, 1);
       }}
-      onMouseEnter={updateWidth}
-      onTouchStart={updateWidth}
       style={
         {
           "--icon-width": `${width.icon}px`,
@@ -385,6 +388,55 @@ function ChatAction(props: {
     </div>
   );
 }
+
+// 让图标显示标题 function ChatAction(props: {
+//   text: string;
+//   icon: JSX.Element;
+//   onClick: () => void;
+// }) {
+//   const iconRef = useRef<HTMLDivElement>(null);
+//   const textRef = useRef<HTMLDivElement>(null);
+//   const [width, setWidth] = useState({
+//     full: 16,
+//     icon: 16,
+//   });
+
+//   function updateWidth() {
+//     if (!iconRef.current || !textRef.current) return;
+//     const getWidth = (dom: HTMLDivElement) => dom.getBoundingClientRect().width;
+//     const textWidth = getWidth(textRef.current);
+//     const iconWidth = getWidth(iconRef.current);
+//     setWidth({
+//       full: textWidth + iconWidth,
+//       icon: iconWidth,
+//     });
+//   }
+
+//   return (
+//     <div
+//       className={`${styles["chat-input-action"]} clickable`}
+//       onClick={() => {
+//         props.onClick();
+//         setTimeout(updateWidth, 1);
+//       }}
+//       onMouseEnter={updateWidth}
+//       onTouchStart={updateWidth}
+//       style={
+//         {
+//           "--icon-width": `${width.icon}px`,
+//           "--full-width": `${width.full}px`,
+//         } as React.CSSProperties
+//       }
+//     >
+//       <div ref={iconRef} className={styles["icon"]}>
+//         {props.icon}
+//       </div>
+//       <div className={styles["text"]} ref={textRef}>
+//         {props.text}
+//       </div>
+//     </div>
+//   );
+// }
 
 function useScrollToBottom(
   scrollRef: RefObject<HTMLDivElement>,
